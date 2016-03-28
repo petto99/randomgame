e023794e1757ad36684c518450941f5c8de8958a
JiHyeon Kim <petto99@naver.com>
2016-03-28 20:10:47 +0900

    #2 add Result Activity

---
+++ app/src/main/java/kr/hs/emirim/petto99/gongyoonogongyoo/ResultActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.petto99.gongyoonogongyoo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by appcreate40 on 2016. 3. 28..
+ */
+public class ResultActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
