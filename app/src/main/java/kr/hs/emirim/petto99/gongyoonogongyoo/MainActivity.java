2041c0533a9051aa7ffc807e1599e3ed3a8aceec
JiHyeon Kim <petto99@naver.com>
2016-04-05 20:32:16 +0900

    #4 handle user input(name)

--- app/src/main/java/kr/hs/emirim/petto99/gongyoonogongyoo/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/petto99/gongyoonogongyoo/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.petto99.gongyoonogongyoo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -35,6 +36,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "잘생겼다!", Toast.LENGTH_LONG).show();        //꼭 show() 써주기
+        String name = mName.getText().toString();   //반드시 toString으로 반환해야돼!!!!!
+
+        if (name.equals("")) {
+            Toast.makeText(this, "이름을 입력해 주세요.", Toast.LENGTH_SHORT).show();        //꼭 show() 써주기
+        }
+        else {
+            Toast.makeText(this, name + "씨, 공유잘생겼죠!!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, ResultActivity.class);
+            startActivity(intent);
+            //여기서 값을 읽어옴 -> 눌렀을때 가져와야 하기 때문에
+        }
+
+//        try{
+//            Toast.makeText(this, name + "씨, 공유잘생겼죠!!!", Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(this, ResultActivity.class);
+//            startActivity(intent);
+//        }catch(NullPointerException e){
+//            Toast.makeText(this, name + "이름을 입력하세요!", Toast.LENGTH_LONG).show();
+//        }catch(Exception e){
+//            Toast.makeText(this, name + "뭔지 모르지만 잘 안되네요!", Toast.LENGTH_LONG).show();
+//        }
     }
 }
